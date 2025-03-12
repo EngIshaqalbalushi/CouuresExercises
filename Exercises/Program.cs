2e2146768d8e97a678ab556c1d5ebf568b60f419
Ishaq Al balushi <ishaqalbalushi693@gmail.com>
2025-03-12 11:22:44 +0400

    Task one

---
+++ Exercises/Program.cs
@@ -0,0 +1,257 @@
+﻿using System;
+
+namespace Exercises
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // check for even and odd numbers
+            //int number;
+
+
+            //Console.WriteLine("Enter a number  ");
+
+            //number =int.Parse (Console.ReadLine());
+
+            //if(number/2== 0)
+            //{
+            //    Console.WriteLine(" The number is Even "+number);
+            //}
+            //else
+            //{
+            //    Console.WriteLine(" The number is odd " + number);
+
+            //}
+            //_______________________________________________________
+
+            //2. Largest of Three Numbers
+
+
+            //int number1, number2, number3;
+
+            //Console.WriteLine("Enter a number one  ");
+            //number1 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter a number two  ");
+            //number2 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter a number three  ");
+            //number3 = int.Parse(Console.ReadLine());
+
+            //if ( number1>number2 && number1>number3)
+            //{
+            //    Console.WriteLine(" a number one is largest   " +number1);
+
+            //}else if(number2>number1 && number2>number2)
+            //{
+            //    Console.WriteLine(" a number tow is largest   " + number2);
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine(" a number three is largest   " + number3);
+
+            //}
+
+            //-------------------------------------------------
+
+            // Temperature Converter
+
+            //double Celsius;
+            //double result;
+            //Console.WriteLine("Enter Celsius number  ");
+            //Celsius = double.Parse(Console.ReadLine());
+
+            //result = (Celsius *9/ 5) + 32;
+
+            //Console.WriteLine(" Temperature in Faherenheit is  "+result);
+            //______________________________________________________________________
+
+
+            // Simple Discount Calculator
+
+            //double Price;
+            //Console.WriteLine("Enter Celsius number  ");
+            //Price = double.Parse(Console.ReadLine());
+
+            //double resut;
+            //double PriceAfterDiscount;
+            //if(Price> 100)
+            //{
+            //    resut = (Price * 10 / 100);
+            //    PriceAfterDiscount = Price - resut;
+            //    Console.WriteLine("The price after discount " +PriceAfterDiscount);
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine("The price = " + Price);
+
+            //}
+
+            //_____________________________________________________________
+
+
+            //Grading System
+
+            //double score;
+            //Console.WriteLine("Enter Student score  ");
+            //score = double.Parse(Console.ReadLine());
+
+            //if (score>=90 && score<=100)
+            //{
+            //    Console.WriteLine("Student score A  ");
+
+            //} else if (score >= 80 && score < 89)
+            //{
+            //    Console.WriteLine("Student score B  ");
+
+            //}
+            //else if (score >= 70 && score < 79)
+            //{
+            //    Console.WriteLine("Student score C  ");
+
+            //}
+            //else if (score >= 60 && score < 69)
+            //{
+            //    Console.WriteLine("Student score D ");
+
+            //}
+            //else if ( score < 60)
+            //{
+            //    Console.WriteLine("Student score F  ");
+
+            //}
+            //else 
+            //{
+            //    Console.WriteLine("Student score not be largest than 100   ");
+
+            //}
+
+            //____________________________________________________________
+
+
+            //Swap Two Numbers
+            //double number1, number2, number3;
+
+            //Console.WriteLine("Enter number 1 ");
+            //number1 = double.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter number 2  ");
+            //number2 = double.Parse(Console.ReadLine());
+
+            //number3 = number1;
+            //number1 = number2;
+            //number2 = number3;
+
+
+            //Console.WriteLine("Enter number one "+number1);
+            //Console.WriteLine("Enter number two "+number2);
+
+            //__________________________________________________
+
+            // Days to Weeks and Days Converter
+
+            //double number1;
+            //int week = 7;
+            //double resut;
+            //double remainingDay;
+
+            //Console.WriteLine("Enter number  ");
+            //number1 = double.Parse(Console.ReadLine());
+
+            //resut = number1 / week;
+
+            //remainingDay= number1 % week;
+            //Console.WriteLine("Weeks   "+resut+"  Days "+remainingDay);
+
+            //________________________________________________________
+
+            //. Electricity Bill Calculator
+
+            //double unit ,reust;
+
+            //Console.WriteLine("Enter number of unit ");
+            //unit = double.Parse(Console.ReadLine());
+
+            //if (unit>300)
+            //{
+
+            //    Console.WriteLine(" number of unit "+unit);
+
+            //}
+            //else if(unit<300)
+            //{
+            //    reust = unit * 0.75;
+            //    Console.WriteLine(" number of unit "+ reust);
+
+            //}
+            //else
+            //{
+            //    reust = unit * 0.5;
+
+            //    Console.WriteLine(" number of unit "+reust);
+
+            //}
+
+
+
+
+
+
+
+            //_____________________________________
+
+            //Simple Calculator
+            //double number1, number2, resut;
+            //string opration;
+
+            //Console.WriteLine("Enter number 1 ");
+            //number1 = double.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter number 2 ");
+            //number2 = double.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Enter your operator(+, -, /, *, %)");
+            //opration = (Console.ReadLine());
+
+
+            //if (opration == "-")
+            //{
+            //    resut = number1 - number2;
+
+            //    Console.WriteLine("resut " + resut);
+
+            //}
+            //else if (opration == "+")
+            //{
+            //    resut = number1 + number2;
+            //    Console.WriteLine("resut " + resut);
+
+            //}
+            //else if (opration == "/")
+            //{
+            //    resut = number1 / number2;
+            //    Console.WriteLine("resut " + resut);
+
+            //}
+            //else if (opration == "*")
+            //{
+            //    resut = number1 * number2;
+            //    Console.WriteLine("resut " + resut);
+            //}
+            //else
+            //{
+            //    Console.WriteLine(" not fond resut");
+
+            //}
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
